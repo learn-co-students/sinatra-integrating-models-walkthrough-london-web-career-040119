6c337cf78a02d9f8bb2aa34198d064ddab61ffc4
hlbtyne <harriet.ballantyne@gmail.com>
2019-04-24 17:09:26 +0100

    Lab passed

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @count_of_words = @analyzed_text.count_of_words
+    @count_of_vowels = @analyzed_text.count_of_vowels
+    @count_of_consonants = @analyzed_text.count_of_consonants
+    @most_used_letter = @analyzed_text.most_used_letter.keys.join("").upcase
+    @most_used_number_times = @analyzed_text.most_used_letter.values.join("")
     erb :results
   end
 end
